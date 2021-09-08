7b61db73d85370d146875f6ea6d5ac56dc967359
Devin Laterza <laterzadj@jacks.sfasu.edu>
2021-09-08 10:20:14 -0500

    Add project files.

---
+++ StoneWoodBooks/Login.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="StoneWoodBooks.Login" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server" >
+    <div id="loginPage">
+        <h1>Login</h1>
+        <asp:Label ID="ulabel" AssociatedControlID="txtUser" runat="server" Text="Username" />
+        <asp:textbox ID="txtUser" runat="server" CssClass="textInput"></asp:TextBox>
+        <br />
+        <asp:Label ID="plabel" AssociatedControlID="txtPassword" runat="server" Text="Password" />
+        <asp:TextBox ID="txtPassword" runat="server" TextMode="Password" ></asp:TextBox>
+        <br />
+        <br />
+        <asp:Button ID="btnSubmitLogin" runat="server" CssClass="btn active" Text="Submit" OnClick="btnSubmitLogin_Click"></asp:Button>
+
+    </div>
+</asp:Content>
---
+++ StoneWoodBooks/Login.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Configuration;
+
+namespace StoneWoodBooks
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSubmitLogin_Click(object sender, EventArgs e)
+        {
+            //input validation - to be replaced by parameterized SQL query
+            /*if (txtUser.Text.Equals("admin") && txtPassword.Text.Equals("admin"))
+            {
+                Response.Redirect("Default.aspx");
+            }*/
+            try
+            {
+                String username = txtUser.Text;
+                String password = txtPassword.Text;
+
+                String query = "SELECT * FROM Users WHERE Username ='" + username + "' and Password='" + password + "'";
+                SqlConnection conn = new SqlConnection();
+                conn.ConnectionString = WebConfigurationManager.ConnectionStrings["DBConnectionString"].ConnectionString;
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Connection = conn;
+
+                //cmd.CommandText = "SELECT Users.Password FROM Users";
+
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.Read())
+                    Response.Redirect("Default.aspx");
+            }
+            catch(Exception)
+            {
+                Response.Write("Error: Invalid input");
+            }
+            /*else
+            {
+                Response.Write("Error: incorrect username or password!");
+            }*/
+        }
+    }
+}
