be3d17d3b32cd4cd1fcb7e3e2903788ec8fe07f1
Devin Laterza <laterzadj@jacks.sfasu.edu>
2021-10-16 10:38:37 -0500

    Added comment to login.aspx.cs

--- StoneWoodBooks/Login.aspx.cs
+++ StoneWoodBooks/Login.aspx.cs
@@ -10,6 +10,7 @@ using System.Web.Configuration;
 
 namespace StoneWoodBooks
 {
+    //added this comment to test git functionality
     public partial class Login : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
