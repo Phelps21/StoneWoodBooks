14554aa9a63b067f00a7dffdf2001de18eeb44f9
Devin Laterza <laterzadj@jacks.sfasu.edu>
2021-11-12 14:57:00 -0600

    updated customer.sql script

--- StoneWoodBooks/Login.aspx.cs
+++ StoneWoodBooks/Login.aspx.cs
@@ -21,10 +21,10 @@ namespace StoneWoodBooks
         protected void btnSubmitLogin_Click(object sender, EventArgs e)
         {
             //input validation - to be replaced by parameterized SQL query
-            /*if (txtUser.Text.Equals("admin") && txtPassword.Text.Equals("admin"))
+            if (txtUser.Text.Equals("admin") && txtPassword.Text.Equals("admin"))
             {
                 Response.Redirect("Default.aspx");
-            }*/
+            }
             try
             {
                 String username = txtUser.Text;
@@ -37,7 +37,7 @@ namespace StoneWoodBooks
                 SqlCommand cmd = new SqlCommand(query, conn);
                 cmd.Connection = conn;
 
-                //cmd.CommandText = "SELECT Users.Password FROM Users";
+                cmd.CommandText = "SELECT Users.Password FROM Users";
 
                 SqlDataReader reader = cmd.ExecuteReader();
                 if (reader.Read())
